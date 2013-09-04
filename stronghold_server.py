92a89352e629dd0b84d65e2c8ab10b2ab2eeedb4
winny <winston@ml1.net>
2013-09-04 02:35:44 -0500

    Can now plot guessed location data on the flask server.

--- stronghold_server.py
+++ stronghold_server.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, url_for, request, send_from_directory
+import pylab
+import io
+import base64
 import stronghold
 
 
 app = Flask(__name__)
 
 
-# @app.route('/plot')
-# def plot():
-#     pass
+@app.route('/plot', methods=['GET', 'POST'])
+def plot():
+    data = {}
+    data['form'] = request.form
+    if request.method == 'POST':
+        data['locations'] = stronghold.guess_locations((float(request.form['x']), float(request.form['z'])))
+        x = []
+        y = []
+        for i in data['locations']:
+            x.append(i[0])
+            y.append(i[1])
+        pylab.scatter(x, y)
+        buf = io.BytesIO()
+        pylab.savefig(buf, format='png')
+        buf.seek(0)
+        data['plot'] = base64.b64encode(buf.read())
+        buf.close()
+    return render_template('plot.html', data=data)
 
 
 @app.route('/locate', methods=['GET', 'POST'])
